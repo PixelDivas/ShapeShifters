389a50c660511f9c9d0e9dd5d63f5bfe00fa4125
Liannys Sanchez <sanchezliannys@gmail.com>
2023-03-31 15:27:59 -0500

    finished set up

---
+++ ShapeShifters/Models/MyContext.cs
@@ -0,0 +1,12 @@
+#pragma warning disable CS8618
+
+using Microsoft.EntityFrameworkCore;
+namespace ShapeShifters.Models;
+
+public class MyContext : DbContext 
+{    
+    public MyContext(DbContextOptions options) : base(options) { }    
+
+   //replace classname with name of model file
+    public DbSet<ClassName> ClassNames { get; set; } 
+}
