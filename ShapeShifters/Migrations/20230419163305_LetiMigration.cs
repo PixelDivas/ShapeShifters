256bd5ab5ea3266b2ee1ceffd6c7c49d9978736a
murielfossi <mina78aaa@gmail.com>
2023-04-19 12:04:14 -0500

    update css

--- ShapeShifters/Migrations/20230419163305_LetiMigration.cs
+++ ShapeShifters/Migrations/20230419163305_LetiMigration.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace ShapeShifters.Migrations
 {
-    public partial class FirstMigration : Migration
+    public partial class LetiMigration : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
